e45316d516ce790ca7dd5b3c6a717c697e656b33
takikawa <takikawa@PC-TAKIKAWA-3.intranet.impressgroup.jp>
2023-04-23 11:20:33 +0900

    1st

---
+++ src/test/java/com/techacademyd/KadaiFirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaiFirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
